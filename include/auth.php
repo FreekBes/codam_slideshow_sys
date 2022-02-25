6e33182e351e94ef2bf1aabede48565403c06998
Freek Bes <freekbladnl@gmail.com>
2022-02-25 15:08:24 +0100

    fix auth realm name

--- include/auth.php
+++ include/auth.php
@@ -2,7 +2,7 @@
 	require_once("settings.php");
 
 	if ($_SERVER['PHP_AUTH_USER'] != DASHBOARD_USERNAME || $_SERVER['PHP_AUTH_PW'] != DASHBOARD_PASSWORD) {
-		header("WWW-Authenticate: Basic realm=\"" . ORGANIZATION_NAME . "\"");
+		header("WWW-Authenticate: Basic realm=\"Announcements Dashboard for " . ORGANIZATION_NAME . "\"");
 		http_response_code(401);
 		die("error:unauthorized");
 	}
