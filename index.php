91bd962550766f8ff59cb9930216f5c867d588c4
Freek Bes <freekbladnl@gmail.com>
2022-03-01 15:12:15 +0100

    remove index.php, redirect now

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<?php require_once("include/auth.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Kodi Monitor Upload Test</title>
-</head>
-<body>
-	<form action="int/upload.php" method="post" target="_self" enctype="multipart/form-data" accept-charset="utf-8" autocomplete="off" name="uploadform">
-		<h1>Kodi Monitor Upload Test</h1>
-		<p>Monitor on the left side</p>
-		<input type="file" name="image" id="image" accept="image/*" />
-		<input type="file" name="video" id="video" accept="video/*" />
-		<input type="submit" value="Upload" />
-	</form>
-</body>
-</html>
+<?php
+	require_once("include/auth.php");
+
+	header("Location: calendar.php");
+	http_response_code(302);
+	die();
+?>
