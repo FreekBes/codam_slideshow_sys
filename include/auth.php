1d8b90f6c26e50e476faebb17653f403d4882f54
Freek Bes <freekbladnl@gmail.com>
2022-03-02 10:25:15 +0100

    fix auth issue

--- include/auth.php
+++ include/auth.php
@@ -1,6 +1,12 @@
 <?php
 	require_once("settings.php");
 
+	if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+		header("WWW-Authenticate: Basic realm=\"Announcements Dashboard for " . ORGANIZATION_NAME . "\"");
+		http_response_code(401);
+		die("error:unauthorized");
+	}
+
 	if ($_SERVER['PHP_AUTH_USER'] != DASHBOARD_USERNAME || $_SERVER['PHP_AUTH_PW'] != DASHBOARD_PASSWORD) {
 		header("WWW-Authenticate: Basic realm=\"Announcements Dashboard for " . ORGANIZATION_NAME . "\"");
 		http_response_code(401);
