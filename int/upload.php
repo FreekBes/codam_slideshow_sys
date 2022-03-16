4adf15c9352b070b730ea42096ed31105c9a0b8a
Freek Bes <freekbladnl@gmail.com>
2022-03-16 16:50:17 +0100

    refactor upload.php

--- int/upload.php
+++ int/upload.php
@@ -6,94 +6,90 @@
 
 	$files = array();
 
-	if (isset($_FILES['media'])) {
-		$file_count = count($_FILES['media']['name']);
-		// handle each file separately in this for loop
-		for ($i = 0; $i < $file_count; $i++) {
-			if ($_FILES['media']['error'][$i] == UPLOAD_ERR_OK && $_FILES['media']['size'][$i] > 0)
-			{
-				$mime = mime_content_type($_FILES['media']['tmp_name'][$i]);
-				if ($mime == "image/gif") {
-					// hacky way of implementing gifs.
-					// since gifs are internally used as a preview for mp4 files, we cannot simply
-					// show a gif on a screen - while saving, it will convert all .gif extensions to
-					// their corresponding .mp4 files.
-					// so, in order to work around that, we convert the gif to an mp4 here.
-					$video_id = time() . "-" . generate_id(4);
-					exec("ffmpeg -i \"" . $_FILES['media']['tmp_name'][$i] . "\" -movflags faststart -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" ../media/$video_id.mp4");
-					$duration = get_video_duration("../media/$video_id.mp4");
-					$new_loc = "media/$video_id-$duration.mp4";
-					if (!rename("../media/$video_id.mp4", "../$new_loc")) {
-						http_response_code(500);
-						die("file_write_fail");
-					}
-					$new_loc_gif = "media/$video_id-$duration.gif";
-					$printable_loc = htmlspecialchars(strip_tags($new_loc_gif));
-					if (!setup_video("../$new_loc", "../$new_loc_gif")) {
-						http_response_code(500);
-						die("video_setup_failed");
-					}
-					array_push($files, $printable_loc);
-				}
-				else if (strstr($mime, "image/")) {
-					$new_loc = "media/" . time() . "-" . generate_id(4) . ".jpeg";
-					$printable_loc = htmlspecialchars(strip_tags($new_loc));
-					// load image into memory (php's gd addon)
-					$input_img = imagecreate_wrapper($_FILES['media']['tmp_name'][$i]);
-					if ($input_img === false) {
-						http_response_code(500);
-						die("file_read_fail");
-					}
-					// resize image to fit on a 1920x1080 screen
-					$output_img = fit_image_for_screen($input_img);
-					// write image to disk with imagejpeg function
-					if (imagejpeg($output_img, "../$new_loc", 95)) {
-						array_push($files, $printable_loc);
-					}
-					else {
-						http_response_code(500);
-						die("file_write_fail");
-					}
-				}
-				else if (strstr($mime, "video/")) {
-					$temp = explode(".", $_FILES['media']['name'][$i]);
-					$ext = array_pop($temp);
-					$video_id = time() . "-" . generate_id(4);
-					$duration = get_video_duration($_FILES['media']['tmp_name'][$i]);
-					if ($duration == 0) {
-						http_response_code(500);
-						die("video_duration_zero");
-					}
-					$new_loc = "media/$video_id-$duration.$ext";
-					$new_loc_gif = "media/$video_id-$duration.gif";
-					$printable_loc = htmlspecialchars(strip_tags($new_loc_gif));
-					// move the uploaded file to the media folder
-					if (move_uploaded_file($_FILES['media']['tmp_name'][$i], "../$new_loc") === true) {
-						if (!setup_video("../$new_loc", "../$new_loc_gif")) {
-							http_response_code(500);
-							die("video_setup_failed");
-						}
-						array_push($files, $printable_loc);
-					}
-					else {
-						http_response_code(500);
-						die("file_write_fail");
-					}
-				}
-				else {
-					http_response_code(400);
-					die("unsupported_file_type");
-				}
-			}
-			else {
-				http_response_code(400);
-				die("file_upload_fail");
-			}
-		}
-	}
-	else {
+	if (!isset($_FILES['media'])) {
 		http_response_code(400);
 		die("media_missing");
+	}
+
+	$file_count = count($_FILES['media']['name']);
+	// handle each file separately in this for loop
+	for ($i = 0; $i < $file_count; $i++) {
+		if ($_FILES['media']['error'][$i] != UPLOAD_ERR_OK || $_FILES['media']['size'][$i] == 0) {
+			http_response_code(400);
+			die("file_upload_fail");
+		}
+		$mime = mime_content_type($_FILES['media']['tmp_name'][$i]);
+		if ($mime == "image/gif") {
+			// hacky way of implementing gifs.
+			// since gifs are internally used as a preview for mp4 files, we cannot simply
+			// show a gif on a screen - while saving, it will convert all .gif extensions to
+			// their corresponding .mp4 files.
+			// so, in order to work around that, we convert the gif to an mp4 here.
+			$video_id = time() . "-" . generate_id(4);
+			exec("ffmpeg -i \"" . $_FILES['media']['tmp_name'][$i] . "\" -movflags faststart -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" ../media/$video_id.mp4");
+			$duration = get_video_duration("../media/$video_id.mp4");
+			$new_loc = "media/$video_id-$duration.mp4";
+			if (!rename("../media/$video_id.mp4", "../$new_loc")) {
+				http_response_code(500);
+				die("file_write_fail");
+			}
+			$new_loc_gif = "media/$video_id-$duration.gif";
+			$printable_loc = htmlspecialchars(strip_tags($new_loc_gif));
+			if (!setup_video("../$new_loc", "../$new_loc_gif")) {
+				http_response_code(500);
+				die("video_setup_failed");
+			}
+			array_push($files, $printable_loc);
+		}
+		else if (strstr($mime, "image/")) {
+			$new_loc = "media/" . time() . "-" . generate_id(4) . ".jpeg";
+			$printable_loc = htmlspecialchars(strip_tags($new_loc));
+			// load image into memory (php's gd addon)
+			$input_img = imagecreate_wrapper($_FILES['media']['tmp_name'][$i]);
+			if ($input_img === false) {
+				http_response_code(500);
+				die("file_read_fail");
+			}
+			// resize image to fit on a 1920x1080 screen
+			$output_img = fit_image_for_screen($input_img);
+			// write image to disk with imagejpeg function
+			if (imagejpeg($output_img, "../$new_loc", 95)) {
+				array_push($files, $printable_loc);
+			}
+			else {
+				http_response_code(500);
+				die("file_write_fail");
+			}
+		}
+		else if (strstr($mime, "video/")) {
+			$temp = explode(".", $_FILES['media']['name'][$i]);
+			$ext = array_pop($temp);
+			$video_id = time() . "-" . generate_id(4);
+			$duration = get_video_duration($_FILES['media']['tmp_name'][$i]);
+			if ($duration == 0) {
+				http_response_code(500);
+				die("video_duration_zero");
+			}
+			$new_loc = "media/$video_id-$duration.$ext";
+			$new_loc_gif = "media/$video_id-$duration.gif";
+			$printable_loc = htmlspecialchars(strip_tags($new_loc_gif));
+			// move the uploaded file to the media folder
+			if (move_uploaded_file($_FILES['media']['tmp_name'][$i], "../$new_loc") === true) {
+				if (!setup_video("../$new_loc", "../$new_loc_gif")) {
+					http_response_code(500);
+					die("video_setup_failed");
+				}
+				array_push($files, $printable_loc);
+			}
+			else {
+				http_response_code(500);
+				die("file_write_fail");
+			}
+		}
+		else {
+			http_response_code(400);
+			die("unsupported_file_type");
+		}
 	}
 
 	http_response_code(201);
