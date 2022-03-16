7b248757d5877134c02c9851efca434458d99d31
Freek Bes <freekbladnl@gmail.com>
2022-03-16 11:37:25 +0100

    add comments

--- js/dragnupload.js
+++ js/dragnupload.js
@@ -40,9 +40,16 @@ function uploadDroppedMedia(files) {
 async function dropUpload(ev) {
 	document.getElementById("loading").style.display = "block";
 	try {
+		// upload the files and wait for this process to complete
 		const mediaUrls = await uploadDroppedMedia(ev.dataTransfer.files);
 		console.log(mediaUrls);
+
+		// add all uploaded media to the available media list
 		addMedia(mediaUrls);
+
+		// add all media to the selected media list
+		// (if placeholder does not exist, this fails without an error on purpose,
+		// since then the files were not dropped on the selected media list)
 		for (var i = 0; i < mediaUrls.length; i++) {
 			insertMediaItemAtPlaceholder(mediaUrls[i]);
 		}
@@ -50,7 +57,6 @@ async function dropUpload(ev) {
 	catch (err) {
 		alert("An error occurred while uploading your file(s).\n\n" + err);
 	}
-	console.log("Removing placeholder now.");
 	removePlaceholder(document.getElementById("drop-location"));
 	document.getElementById("loading").style.display = "none";
  }
